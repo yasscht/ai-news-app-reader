4c5a1589a7ca4d51885a15a2c60c8a003a6d9e50
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-16 17:58:50 +0000

    alan ai integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
 function App() {
-  return <div className="App">hi</div>;
+  const alanKey = process.env.REACT_APP_ALAN_KEY;
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ commande }) => {
+        if (commande === "testCommand") {
+          alert("this code was executed");
+        }
+      },
+    });
+  }, []);
+  return (
+    <div className="App">
+      <h1>AI News Application</h1>
+    </div>
+  );
 }
 
 export default App;
